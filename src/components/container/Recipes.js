977f7fd255564e9a23246e74ae128f02ac5bf74b
Edward Iga Kigongo <igakigongo@gmail.com>
2020-10-07 04:52:09 +0200

    refactor - move no-recipes-found component into separate file

--- src/components/container/Recipes.js
+++ src/components/container/Recipes.js
@@ -2,6 +2,7 @@ import React, { useEffect, useLayoutEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import api from '../../api';
+import NoRecipesFound from '../presentational/NoRecipesFound';
 import Time from '../presentational/Time'
 
 const selectedIngredients = ingredients => ingredients.filter(x => x.checked);
@@ -21,45 +22,38 @@ const RecipesList = ({ ingredients }) => {
     });
   }, [ingredients]);
 
-  return (recipes.length === 0 ? (
+  return (recipes.length === 0 ? (<NoRecipesFound />) : (
     <div className="container-fluid">
-      <div className="alert alert-info">
-        Information:&nbsp;
-        No recipes found.
+      <div className="row recipes-grid">
+        {recipes.map(recipe => {
+          const {
+            id, image, readyInMinutes, title,
+          } = recipe;
+          let { imageType } = recipe;
+          const imageTokens = image.split('.');
+          if (typeof imageType === 'undefined') {
+            (imageType = imageTokens[imageTokens.length - 1]);
+          }
+
+          return (
+            <div className="col col-lg-2 recipe-item" key={id}>
+              <div className="card mb-3 shadow">
+                <img alt={image} className="card-img-top" src={`https://spoonacular.com/recipeImages/${id}-480x360.${imageType}`} />
+                <div className="card-body bg-warning">
+                  <b>{title}</b>
+                </div>
+                {readyInMinutes && (
+                  <div className="card-footer bg-dark text-white">
+                    <Time minutes={readyInMinutes} />
+                  </div>
+                )}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
-  ) : (
-      <div className="container-fluid">
-        <div className="row recipes-grid">
-          {recipes.map(recipe => {
-            const {
-              id, image, readyInMinutes, title,
-            } = recipe;
-            let { imageType } = recipe;
-            const imageTokens = image.split('.');
-            if (typeof imageType === 'undefined') {
-              (imageType = imageTokens[imageTokens.length - 1]);
-            }
-
-            return (
-              <div className="col col-lg-2 recipe-item" key={id}>
-                <div className="card mb-3 shadow">
-                  <img alt={image} className="card-img-top" src={`https://spoonacular.com/recipeImages/${id}-480x360.${imageType}`} />
-                  <div className="card-body bg-warning">
-                    <b>{title}</b>
-                  </div>
-                  {readyInMinutes && (
-                    <div className="card-footer bg-dark text-white">
-                      <Time minutes={readyInMinutes} />
-                    </div>
-                  )}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    ));
+  ));
 };
 
 RecipesList.propTypes = {
---
+++ src/components/presentational/NoRecipesFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoRecipesFound = () => <div className="container-fluid">
+  <div className="alert alert-info">
+    Information:<span className="mx-2 font-weight-bold">No recipes found.</span>
+  </div>
+</div>;
+
+export default NoRecipesFound;
